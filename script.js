375776a457f677713e4b65375b757c19f9804c20
Jinu Vijayan <jinuvijayan01@gmail.com>
2024-02-28 20:42:10 +0530

    Change: changed from using componenets to React.createElement

--- script.js
+++ script.js
@@ -1,34 +1,19 @@
-const FirstDiv = () => {
-    return (
-        <div className = "div-margin">
-            <h1>Topics covered</h1>
-            <p className = "p-margin">The following is a list of all the topics we cover in the MDN learning area.</p>
-        </div>
-    )
-}
+const h1 = React.createElement("h1",{},"Topics covered");
+const firstDivPara = React.createElement("p",{className:"p-margin"},"The following is a list of all the topics we cover in the MDN learning area.");
+const firstDiv = React.createElement("div",{className:"div-margin"},[h1,firstDivPara]);
 
-const DataDiv = ({href, anchorText, paraText, paraClass}) => {
-    return(
-        <div className = "div-margin">
-            <a href = {href}>{anchorText}</a>
-            <p className = {paraClass}>{paraText}</p>
-        </div>
-    )
-}
+const secondDivAnchor = React.createElement("a",{href:"https://developer.mozilla.org/en-US/docs/Learn/Getting_started_with_the_web"},"Getting started with the web");
+const secondDivPara = React.createElement("p",{className:"p-margin margin-left"},"Provides a practical introduction to web development for complete beginners.");
+const secondDiv = React.createElement("div",{className:"div-margin"},[secondDivAnchor,secondDivPara]);
 
-const Data = () => {
-    return(
-        <div>
-            <FirstDiv/>
+const thirdDivAnchor = React.createElement("a",{href:"https://developer.mozilla.org/en-US/docs/Learn/HTML"},"HTML — Structuring the web");
+const thirdDivPara = React.createElement("p",{className:"p-margin margin-left"},"HTML is the language that we use to structure the different parts of our content and define what their meaning or purpose is. This topic teaches HTML in detail.");
+const thirdDiv = React.createElement("div",{className:"div-margin"},[thirdDivAnchor,thirdDivPara]);
 
-            <DataDiv href = "https://developer.mozilla.org/en-US/docs/Learn/Getting_started_with_the_web" anchorText= "Getting started with the web" paraText = "Provides a practical introduction to web development for complete beginners." paraClass = "p-margin margin-left"/>
+const fourthDivAnchor = React.createElement("a",{href:"https://developer.mozilla.org/en-US/docs/Learn/CSS"},"CSS — Styling the web");
+const fourthDivPara = React.createElement("p",{className:"p-margin margin-left"},"CSS is the language that we use to control our web content's style and layout, as well as adding behavior like animation. This topic provides comprehensive coverage of CSS.");
+const fourthDiv = React.createElement("div",{className:"div-margin"},[fourthDivAnchor,fourthDivPara]);
 
-            <DataDiv href = "https://developer.mozilla.org/en-US/docs/Learn/HTML" anchorText= "HTML — Structuring the web" paraText = "HTML is the language that we use to structure the different parts of our content and define what their meaning or purpose is. This topic teaches HTML in detail." paraClass = "p-margin margin-left"/>
+const div = React.createElement("div",{},[firstDiv,secondDiv,thirdDiv,fourthDiv]);
 
-            <DataDiv href = "https://developer.mozilla.org/en-US/docs/Learn/CSS" anchorText= "CSS — Styling the web" paraText = "CSS is the language that we use to control our web content's style and layout, as well as adding behavior like animation. This topic provides comprehensive coverage of CSS." paraClass = "p-margin margin-left"/>
-            
-        </div>
-    )
-}
-
-ReactDOM.render(<Data/>, document.querySelector("#root"))
+ReactDOM.render(div, document.querySelector("#root"));
