7558b71c0c7bb36d892b455d10d2cb1f219cbd2a
Jinu Vijayan <jinuvijayan01@gmail.com>
2024-02-28 07:10:55 +0530

    Add:added styling to the components

--- script.js
+++ script.js
@@ -1,36 +1,38 @@
 const FirstDiv = () => {
     return (
-        <div>
+        <div className = "div-margin">
             <h1>Topics covered</h1>
-            <p>The following is a list of all the topics we cover in the MDN learning area.</p>
+            <p className = "p-margin">The following is a list of all the topics we cover in the MDN learning area.</p>
         </div>
     )
 }
 
 const SecondDiv = () => {
     return (
-        <div>
+        <div className = "div-margin">
             <a href="https://developer.mozilla.org/en-US/docs/Learn/Getting_started_with_the_web">Getting started with the web</a>
 
-            <p>Provides a practical introduction to web development for complete beginners.</p>
+            <p className = "p-margin margin-left">Provides a practical introduction to web development for complete beginners.</p>
         </div>
     )
 }
 
 const ThirdDiv = () => {
     return (
-        <div>
+        <div className = "div-margin">
             <a href = "https://developer.mozilla.org/en-US/docs/Learn/HTML">HTML — Structuring the web</a>
-            <p>HTML is the language that we use to structure the different parts of our content and define what their meaning or purpose is. This topic teaches HTML in detail.</p>
+            
+            <p className = "p-margin margin-left">HTML is the language that we use to structure the different parts of our content and define what their meaning or purpose is. This topic teaches HTML in detail.</p>
         </div>
     )
 }
 
 const FourthDiv = () => {
     return (
-        <div>
+        <div className = "div-margin">
             <a href = "https://developer.mozilla.org/en-US/docs/Learn/CSS">CSS — Styling the web</a>
-            <p>CSS is the language that we use to control our web content's style and layout, as well as adding behavior like animation. This topic provides comprehensive coverage of CSS.</p>
+
+            <p className = "p-margin margin-left">CSS is the language that we use to control our web content's style and layout, as well as adding behavior like animation. This topic provides comprehensive coverage of CSS.</p>
         </div>
     )
 }
